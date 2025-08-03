c1c93fd2e05c8a0157eb92d1ab4c216a958fddb5
Valrandy·Joses <a3159398370@gmail.com>
2025-08-03 14:43:05 +0800

    更新文件

---
+++ code/40 ip地址.py
@@ -0,0 +1,14 @@
+import requests
+url = 'https://ip.900cha.com/'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
+}
+data = {
+    'wd': 'ip'
+}
+proxy={
+    'http': '123.139.74.250',
+}
+response = requests.get(url=url, params=data, headers=headers,proxy= proxy)
+with open('40ip.text','w',encoding='utf-8')as fp:
+    fp.write(response.text)
